36da1de6b886b05c59dc45228498a87db94c9fe2
Ximena Prado Blanco <41089488+ximenaprado@users.noreply.github.com>
2026-03-29 20:55:45 -0300

    Add functions for calculating power of a base
    Define functions for calculating power using iterative and recursive methods.

---
+++ Semana5/ejercicio5.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 5:
+Calcular la potencia de una base elevada a un exponente entero positivo.
+"""
+
+def potencia_ciclo(base, exponente):
+    pass
+
+
+def potencia_recursiva(base, exponente):
+    pass
